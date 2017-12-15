082eedc477862b3a28d25a0e85534d04927556c3
Samuel Doud <sd8396a@student.american.edu>
2017-12-14 23:36:03 -0500

    There is a lot here...

--- PLR.py
+++ PLR.py
@@ -1,12 +1,13 @@
 import re
+import random
 
 import Player
 
 class PLR(object):
     def __init__(self, mod=12):  
-        self.function_unravel_re = re.compile(r"(\([PLR]+\)[0-9]+)|([PLR]+)")
+        self.function_unravel_re = re.compile(r"(\([\[\]PLR]+\)[0-9]+)|([\[\]PLR]+)")
         self.function_unravel_power_re = re.compile(r"[0-9]+")
-        self.function_unravel_extract_re = re.compile(r"[PLR]*")
+        self.function_unravel_extract_re = re.compile(r"[\[\]PLR]*")
         self.mod_int = mod
         self.function_mapping = {"P":self.P, "L":self.L, "R":self.R}
       
@@ -28,11 +29,37 @@ class PLR(object):
     def modulo(self, n):
         return n % self.mod_int
 
+    def order(self, chord, function_str, max_index=100, make_music=True):
+        name = "-".join(function_str)
+        chord_start_list = [chord] +  [self.transform(f, chord) for f in function_str[1:]]
+        function_str = function_str[0]
+        encounter = []
+        new_chord = chord
+        chord_index = 0
+        while chord_index < len(chord_start_list) and len(encounter) < max_index:
+            encounter.append(new_chord)
+            new_chord = self.transform(function_str, new_chord)
+            if new_chord == chord_start_list[chord_index]:
+                chord_index += 1
+                if chord_index < len(chord_start_list):
+                    new_chord = chord_start_list[chord_index]
+        if make_music:
+            self.make_music(name, chord, chords_override=encounter)
+        return len(encounter)
+            
+
     def transform(self, function, start_chord, all_chords=False):
         #read Left to Right
         chord_map = [start_chord]
-        for key in reversed(self.function_unravel(function.upper())):
-            chord_map.append(self.function_mapping[key](chord_map[-1]))
+        last = 0
+        for key in self.function_unravel(function.upper())[::-1]:
+            if key == "[":
+                chord_map = chord_map[:-last] + [chord_map[-1]]
+            elif key == "]":
+                last = 0
+            else:
+                last += 1
+                chord_map.append(self.function_mapping[key](chord_map[-1]))
         if not all_chords:
             return chord_map[-1]
         return chord_map
@@ -47,12 +74,15 @@ class PLR(object):
             res_funct += function * (int(power[0]) if power else 1)
         return res_funct
 
-    def make_music(self, function, base_chord):
-        results = self.transform(function, base_chord, all_chords=True)
-        notes = []
-        for result in results:
-            notes += result.to_notes()
-        Player.make_wav_from_notes(notes, str(base_chord.chord_name()) + "-" + str(function))
+    def make_music(self, function, base_chord, chords_override=None):
+        if not chords_override:
+            name = base_chord.chord_name() + "-" +str(function)
+            chords =self.transform(function, base_chord, all_chords=True)
+        else:
+            name = base_chord.chord_name() + "-subgroup-" + (function)
+            chords = chords_override
+        chords_list = [res.to_notes() for res in chords]
+        Player.make_wav_from_notes(chords_list, "music/" + name)
 
     def __len__(self):
         return 0
@@ -65,6 +95,7 @@ class Chord(object):
         self.x = x
         self.y = y
         self.z = z
+        self.p, self.l, self.r = None, None, None
         self.chord_map = {}
         self.construct()
 
@@ -91,6 +122,23 @@ class Chord(object):
     def order(self):
         pass
 
+    def find_function(self, chord_to, encountered=[], funct_str="", max_length=10):
+        """Given a chord, find a set of functions that links the two notes."""
+        plr = PLR(12)
+        functions = {"P": plr.P,"L": plr.L,"R": plr.R}
+        function_strs = []
+        if self in encountered or len(funct_str) > max_length:
+            return [""]
+        new_encountered = list(encountered) + [self]
+        if self == chord_to:
+            return [funct_str]
+        for name in functions:
+            new_chord = functions[name](self)
+            function_strs.extend(new_chord.find_function(chord_to,
+                                                         new_encountered, funct_str=name+funct_str))
+        return sorted([f_str for f_str in function_strs if f_str], key=len)
+
+
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")"
 
@@ -100,19 +148,77 @@ class Chord(object):
     def add_to(tup, add, mod=12):
         return tuple((((x + add) % mod) for x in tup))
 
+
 #this is the PLR function object, you define the mod here
 PLR_test = PLR(12)
 #this is a sample chord
+'''
 chord_test = Chord(0, 4, 7)
 #Just showing how functions are written
 print(PLR_test.function_unravel("PLR(PL)4PPPL(LR)2L"))
 print(PLR_test.L(chord_test))
 #you can transform a chord with a function string
-results = PLR_test.transform("PLR(PL)4PPP", chord_test, all_chords=True)
+results = PLR_test.transform("P[LR](PL)4PPP", chord_test, all_chords=True)
 notes = []
 for result in results:
     print(result.to_notes())
     notes += result.to_notes()
+'''
+C = Chord(0, 4, 7)
+Gm = Chord(2, 10, 7)
+Bb = Chord(10, 2, 5)
+
+print(PLR_test.order(C, "[PLR]"))
+functs_to_Gm = C.find_function(Gm)
+functs_to_Bb = C.find_function(Bb)
+
+for funct in functs_to_Gm:
+    print(funct + ", " + str(len(funct)))
+'''
+print("-" * 80)
+for funct in functs_to_Bb:
+    print(funct + ", " + str(len(funct)))
 
 #Player.make_wav_from_notes(notes, "test_construction")
-PLR_test.make_music("(LR)9", Chord(0,8,5))
+PLR_test.make_music("[LP](LR)9", Chord(0,8,5))'''
+
+
+
+#Producing subgroups
+C = Chord(0, 4, 7)
+'''
+#Order 2
+PLR_test.order(C, ["L"])
+PLR_test.order(C, ["(LR)6"])
+for n in range(12):
+    PLR_test.order(C, ["(LR)" + str(n) + "L"])
+
+#Order 4
+PLR_test.order(C, ["(LR)3"])
+for n in range(6):
+    PLR_test.order(C, ["(LR)6", "(LR)" + str(n) + "L"])
+
+#Order 6
+
+PLR_test.order(C, ["(LR)2"])
+for n in range(4):
+    PLR_test.order(C, ["(LR)4", "(LR)" + str(n) + "L"])
+
+
+#Order 8
+for n in range(3):
+    PLR_test.order(C, ["(LR)3", "(LR)" + str(n) + "L"])
+
+#Order 12
+
+PLR_test.order(C, ["LR"])
+PLR_test.order(C, ["(LR)2", "L"])
+PLR_test.order(C, ["(LR)2", "(LR)L"])
+
+
+#Order 3
+PLR_test.order(C, ["(LR)4"])
+
+#Order 24
+PLR_test.order(C, ["LR", "L"])
+'''
--- Player.py
+++ Player.py
@@ -1,17 +1,24 @@
+import itertools
+
 import numpy
-import pysynth_b as ps
-from pysynth_b import *
+import pysynth_d as ps
+from pysynth_d import *
 import wave
 from pydub import AudioSegment
 
 
 def make_wav_from_notes(notes, filename):
-	sounds = []
-	filename = filename.replace(".wav", "") + ".wav"
-	note_play = tuple([(n, 4) for n in notes])
-	for i in range(3):
-		name = "sound_" + str(i) +".wav"
-		ps.make_wav(note_play[i::3], fn=name)
-		sounds.append(AudioSegment.from_wav(name))
-	combined = (sounds[0].overlay(sounds[1])).overlay(sounds[2])
-	combined.export(filename, format="wav")
+    """Creates a .wav file from a set of notes.
+    The notes are taken in groups of 3s and a .wav file
+    is created from each of those. Then these files are
+    stitched together."""
+    sounds = []
+    filename = filename.replace(".wav", "") + ".wav"
+    note_play = tuple([(n, 1) for n in itertools.chain(*notes)])
+
+    for i in range(3):
+        name = "sound_" + str(i) + ".wav"
+        ps.make_wav(note_play[i::3], fn=name)
+        sounds.append(AudioSegment.from_wav(name))
+    combined = (sounds[0].overlay(sounds[1])).overlay(sounds[2])
+    combined.export(filename, format="wav")
